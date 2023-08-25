d5851ec2e817b9314154da4b31ee22277dd18cfa
DiegoSt23 <diego.stonerough@gmail.com>
2023-08-25 10:12:44 -0600

    Update Delta icon

---
+++ src/components/organisms/Tabs/Tabs.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+interface TabProps {
+  title: string;
+  icon: ReactNode;
+  children: ReactNode;
+}
+
+export interface TabsProps {
+  items: TabProps[];
+}
+
+export const Tabs = () => {
+  <div></div>
+};
